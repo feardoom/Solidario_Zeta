88ba4f545cedcb0ef2d421e81b094b7f4b8910d9
UserPL135Pc02 <UserPL135Pc02@SLFRPL135PC02.ensino.atec.local>
2017-01-27 15:34:49 +0000

    classes criadas

--- Solidario_Zeta/src/model/Requisicao.java
+++ Solidario_Zeta/src/model/Requisicao.java
@@ -1,7 +1,23 @@
 package model;
 
+import java.util.Date;
+
 public class Requisicao {
-	int id_requisicao;
-	iRequisitavel itemRequisitado;
-	Utilizador requisitante;
+	public int id_requisicao;
+	protected iRequisitavel itemRequisitado;
+	protected Utilizador requisitante;
+	protected Date dataRequisicao;
+	protected Date dataInicio;
+	protected Date dataFim;
+	protected String motivo;
+	
+	public Requisicao() {
+		
+	}
+	
+	protected boolean requisitar(iRequisitavel item, Utilizador requisitante) {
+		
+		return true;
+	}
+	
 }
